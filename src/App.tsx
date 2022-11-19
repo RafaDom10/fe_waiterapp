3143a4c6896d9a98be17771ce93761a8d26f2665
Rafael Domingues <r-domingues@outlook.com>
2022-11-19 00:01:53 -0300

    added orders components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Header } from './components/Header'
 import { GlobalStyles } from './styles/GlobalStyles'
+import { Orders } from './components/Orders/index'
 
 export function App (): JSX.Element {
   return (
     <>
       <GlobalStyles />
       <Header />
+      <Orders />
     </>
   )
 }
